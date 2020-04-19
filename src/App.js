5efda3d9b85c29680a85d5b87796c777b489e8af
benolayinka <ben.olayinka@gmail.com>
2020-04-19 19:04:25 +0200

    bit npmrc

--- src/App.js
+++ src/App.js
@@ -3,52 +3,92 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { setCameraToBoundingBox } from '@bit/benolayinka.benolayinka.utils'
+import { setContent } from '@bit/benolayinka.benolayinka.utils'
 import ThreeSceneRenderer from '@bit/benolayinka.benolayinka.three-scene-renderer'
-import BasicSceneBuilder from '@bit/benolayinka.benolayinka.basic-scene-builder'
 
 function App() {
 
   	var canvas, renderer, scene, camera, clock, mixer, controls
 
   	function extendScene(props){
-		({canvas, renderer, scene, camera, controls} = props)
+
+		({canvas, renderer} = props)
+
+		scene = new THREE.Scene()
+
+	  	camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
+
+	  	controls = new OrbitControls(camera, renderer.domElement);
 
 		clock = new THREE.Clock()
 
 	  	scene.background = new THREE.Color('red');
 
+	  	var avatar = new THREE.Object3D()
+
+	  	scene.add(avatar)
+
 	  	let loader = new GLTFLoader()
+
 	  	loader.load('assets/coach-samba.glb', (gltf)=>{
+
+	  		//fix for Box3.setFromObject doesn't calc bounding boxes
+	  		gltf.scene.traverse((mesh)=>{
+
+				if(mesh.isSkinnedMesh) {
+					const helper = new THREE.BoxHelper(mesh);
+					helper.visible = false
+					avatar.add(helper)
+				}
+	  		})
+
 			mixer = new THREE.AnimationMixer( gltf.scene );
+
 	 		var action = mixer.clipAction( gltf.animations[ 0 ] );
+
 	  		action.play();
+
 			scene.add(gltf.scene)
-			setCameraToBoundingBox(camera, gltf.scene)
+
+			setContent(avatar, camera, controls)
 	  	})
 
-	  	let spotLight = new THREE.SpotLight(0xffffff, 0.25)
+	  	let spotLight = new THREE.SpotLight(0xffffff, 1)
 	  	spotLight.position.set(45, 50, 15);
 	  	scene.add(spotLight);
 
-	  	let ambLight = new THREE.AmbientLight(0xffffff);
+	  	let ambLight = new THREE.AmbientLight(0xffffff, 0.5);
 	  	ambLight.position.set(5, 3, 5);
 	  	scene.add(ambLight);
+
+	  	window.addEventListener('resize', handleWindowResize)
+
+	  	handleWindowResize()
 
 	  	animate()
   	}
 
+  	function handleWindowResize(){
+		let width = canvas.clientWidth;
+		let height = canvas.clientHeight;
+		camera.aspect = width/height;
+		camera.updateProjectionMatrix();
+	}
+
   	function animate(){
+
 		var delta = clock.getDelta();
 
 		if(mixer){
 	  		mixer.update(delta)
 		}
 
+    	controls.update();
+
+		renderer.render(scene, camera)
+
 		requestAnimationFrame(animate)
   	}
-
-  	const sb = new BasicSceneBuilder({onBuild: extendScene})
 
 	return (
 		<div className="App">
